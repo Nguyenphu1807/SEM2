25f6d66d1389c2e12320224b7b661b34e73b1308
Nguyenphu1807 <54586883+Nguyenphu1807@users.noreply.github.com>
2020-05-21 14:17:17 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap ho ten");
+            String name = Console.ReadLine();
+            Console.WriteLine("Nhap dia chi");
+            String diachi = Console.ReadLine();
+            Console.WriteLine("Nhap so dien thoai");
+            String phone = Console.ReadLine();
+
+            Console.WriteLine("Thong tin nguoi dung");
+            Console.WriteLine("Ho ten : " + name);
+            Console.WriteLine("Dia chi : " + diachi);
+            Console.WriteLine("Phone : " + phone);
+        }
+    }
+}

2baddd32c257601b6d453a5897420a2c4e8f13fb
Nguyenphu1807 <54586883+Nguyenphu1807@users.noreply.github.com>
2020-05-25 22:43:31 +0700

    lab3

---
+++ Window.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Window
+{
+    public Window(int top, int left)
+    {
+        this.top = top;
+        this.left = left;
+    }
+    public virtual void DrawWindow()
+    {
+        Console.WriteLine("Window: drawing Window at {0}, {1}",
+        top, left);
+    }
+    protected int top;
+    protected int left;
+}
